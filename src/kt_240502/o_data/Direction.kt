59d5cc260c923777785d9ee306677ce9b5cb3978
Hwangsj-qk <sujeong7191@naver.com>
2024-05-03 12:29:57 +0900

    enum 공부

---
+++ src/kt_240502/o_data/Direction.kt
@@ -0,0 +1,34 @@
+package kt_240502.o_data
+
+enum class Direction (val code: String) {
+    NORTH("북"), SOUTH("서"), EAST("동"), WEST("서")
+
+}
+
+fun main() {
+    val north = Direction.NORTH
+    println(north.code)             // 북
+    println(north)                  // NORTH
+
+    val direction = Direction.SOUTH
+    if(direction == Direction.SOUTH) {
+        println("${direction.code}쪽으로 갑니다. ")
+    }
+    if(direction == Direction.NORTH) {
+        // 북쪽 관련 로직...
+    }
+
+    // Enum 클래스와 when 문을 함께 사용
+    handleDirection(Direction.SOUTH)
+}
+
+// Enum 클래스를 매개변수로 받아 활용하는 메서드
+private fun handleDirection(direction: Direction){
+    when (direction) {
+        Direction.NORTH -> println("북쪽")
+        Direction.SOUTH -> println("남쪽")
+        Direction.EAST -> println("동쪽")
+        Direction.WEST -> println("서쪽")
+    }
+}
+
